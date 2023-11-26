2a3ab449ed6542a99fcc258c039d345d9ebb5328
Isabel Lacerda <contato.lacerdaisab@outlook.com>
2023-11-25 23:59:54 -0300

    add: changes on project descriptions

--- aula-10-ui/src/App.js
+++ aula-10-ui/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/login/index.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ListarTarefa />
+      {/* <Login /> */}
     </div>
   );
 }
--- aula-10-ui/src/pages/tarefa/ListarTarefa.jsx
+++ aula-10-ui/src/pages/tarefa/ListarTarefa.jsx
@@ -33,12 +33,11 @@ function createData(
 
 //Definição do array contendo os dados iniciais da listagem de tarefas
 const initialRows = [
-  createData(1, 'Tarefa 1', 'Descrição da Tarefa 1', '2022-01-01', '2022-01-02', 'Concluída', 'Recurso 1'),
-  createData(2, 'Tarefa 2', 'Descrição da Tarefa 2', '2022-01-03', '2022-01-04', 'Em Andamento', 'Recurso 2'),
-  createData(3, 'Tarefa 3', 'Descrição da Tarefa 3', '2022-01-04', '2022-01-05', 'Em Andamento', 'Recurso 3'),
-  createData(4, 'Tarefa 4', 'Descrição da Tarefa 4', '2022-01-05', '2022-01-06', 'Em Andamento', 'Recurso 4'),
-  createData(5, 'Tarefa 5', 'Descrição da Tarefa 5', '2022-01-06', '2022-01-07', 'Em Andamento', 'Recurso 5'),
-  createData(6, 'Tarefa 6', 'Descrição da Tarefa 6', '2022-01-07', '2022-01-08', 'Aguardando', 'Recurso 6'),
+  createData(1, 'Tarefa 1', 'Atividade Prática 1', '2022-01-01', '2022-01-02', 'Concluída', 'Recurso 1'),
+  createData(2, 'Tarefa 2', 'Atividade Prática 2', '2022-01-03', '2022-01-04', 'Em Andamento', 'Recurso 2'),
+  createData(3, 'Tarefa 3', 'Exercícios', '2022-01-04', '2022-01-05', 'Em Andamento', 'Recurso 3'),
+  createData(4, 'Tarefa 4', 'Revisão 1', '2022-01-05', '2022-01-06', 'Em Andamento', 'Recurso 4'),
+  createData(6, 'Tarefa 6', 'Revisão 2', '2022-01-07', '2022-01-08', 'Aguardando', 'Recurso 6'),
 ];
 
 //Componente ListarTarefa
@@ -86,7 +85,7 @@ const ListarTarefa = () => {
     <Card>
         <CardHeader
           title="Tarefas"
-          subheader="Listagem de Tarefas"
+          subheader="Insira os trabalhos desse trimestre"
         /> 
         <CardContent>
             <TableContainer component={Paper}>
